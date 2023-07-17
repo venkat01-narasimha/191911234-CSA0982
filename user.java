c76c1706c18d81a8a21642a92d2c2573ba27df77
venkat01-narasimha <122338692+venkat01-narasimha@users.noreply.github.com>
2023-07-17 05:48:14 +0530

    Add files via upload

---
+++ user.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class user
+{
+public static void main(String[]args)
+{
+Scanner input=new Scanner(System.in);
+String s1=input.nextLine();
+String s2=input.nextLine();
+if(s1.equals(s2))
+ System.out.print("user name valid");
+else
+ System.out.print("user name Invalid");
+}
+}
