c1536d6acd5a7d021bd9d4a6a7a44a2b6fc57f79
venkat01-narasimha <122338692+venkat01-narasimha@users.noreply.github.com>
2023-07-17 05:30:14 +0530

    Add files via upload

---
+++ userv.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class userv{
+public static void main(String[]args){
+Scanner input=new Scanner(System.in);
+String s1=input.nextLine();
+String s2=input.nextLine();
+if(s1.equals(s2)){
+System.out.println("valid");
+}
+else{
+System.out.println("invalid");
+}
+}
+}
