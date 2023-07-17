2569bb0787b241894531ed1f37adc43234ddebf3
venkat01-narasimha <122338692+venkat01-narasimha@users.noreply.github.com>
2023-07-17 05:43:18 +0530

    Add files via upload

---
+++ posneg.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class posneg{
+public static void main(String[]args){
+Scanner sc=new Scanner(System.in);
+int p=0,n=0,i=0,j=0;
+while(n!=-1){
+ int nu=sc.nextInt();
+if(n==-1){
+break;}
+else{
+if(n>0){
+p=p+nu;
+i++;}
+else{
+n=n+nu;
+j++;}
+}}
+System.out.println(p/i);
+
+System.out.println(p/i);
+System.out.println(n/j);
+}
+}
