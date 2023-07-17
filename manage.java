cfb6428c6722811841ed6c2674538a9900301d1b
venkat01-narasimha <122338692+venkat01-narasimha@users.noreply.github.com>
2023-07-17 05:44:23 +0530

    Add files via upload

---
+++ manage.java
@@ -0,0 +1,12 @@
+class manage
+{
+public static void main(String[]args)
+{
+String studentname="harsha";
+int reg=192011364;
+String location="chennai";
+System.out.println("Student Name:"+studentname);
+System.out.println("Student Reg no. :"+reg);
+System.out.println("Student Located in:"+location);
+}
+}
