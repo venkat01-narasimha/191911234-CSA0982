7c37431e632714517fa257cc6f173dd1695564d2
venkat01-narasimha <122338692+venkat01-narasimha@users.noreply.github.com>
2023-07-17 05:49:33 +0530

    Add files via upload

---
+++ vote.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class vote
+{
+public static void main(String []args)
+{
+Scanner a=new Scanner(System.in);
+int age=a.nextInt();
+if(age>18)
+System.out.println("eligible");
+else
+System.out.println("not eligible ");
+}
+}
