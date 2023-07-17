3dbff05bb128e164462e072583719f93fd16b494
venkat01-narasimha <122338692+venkat01-narasimha@users.noreply.github.com>
2023-07-17 05:48:56 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,12 @@
+class sum
+{
+public static void main(String[]arg)
+{
+int a=100;
+int b=5;
+System.out.println(a+b);
+System.out.println(a-b);
+System.out.println(a*b);
+System.out.println(a/b);
+}
+}
