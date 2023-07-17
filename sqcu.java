9d574c6e9b4425bce9a20fbea969a19ae8689271
venkat01-narasimha <122338692+venkat01-narasimha@users.noreply.github.com>
2023-07-17 05:46:11 +0530

    Add files via upload

---
+++ sqcu.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class sqcu{
+public static void main(String[]args){
+Scanner input=new Scanner(System.in);
+float n=input.nextFloat();
+System.out.println("square"+n*n);
+System.out.println("cube"+n*n*n);
+}
+}
